8d091032c7209f32f87533511e040fdba5c47a1d
stokely <adrienne.j.abrams@gmail.com>
2019-02-02 13:15:44 -0600

    working test routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,31 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+
+  state = {
+    "testValue": "getting..."
+  }
+
+
+
+
+  componentDidMount() {
+    // console.log("mouting app");
+    // axios.get("/api/test", (result) => {
+    //   console.log(result);
+    // })
+
+    axios.get("/api/test")
+  .then(result => {
+    console.log(result.data.test)
+    this.setState({"testValue": result.data.test})
+  })
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -11,7 +34,8 @@ class App extends Component {
           <h2>Welcome to Final! React! Project!</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {/* To get started, edit <code>src/App.js</code> and save to reload. */}
+          the test value is: {this.state.testValue}
         </p>
       </div>
     );
--- server.js
+++ server.js
@@ -12,6 +12,9 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
+app.get("/api/test", function(req, res) {
+  res.json({"test": "value"})
+});
 
 // Send every other request to the React app
 // Define any API routes before this runs
