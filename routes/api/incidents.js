0fdf155f4b636dddbbc46212754a74310e62f2e7
stokely <adrienne.j.abrams@gmail.com>
2019-02-09 13:54:05 -0600

    removed axios

--- routes/api/incidents.js
+++ routes/api/incidents.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const axios = require("axios");
 const incidentsController = require("../../controllers/incidentsController");
 
 // Matches with "/api/incidents"
