f0163ad0db601716d3c7de52582323375b9989e2
Elizabeth Adamczyk <emadamczyk@hotmail.com>
2019-02-16 11:11:18 -0600

    testing mdbootstrap

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,9 +1,17 @@
 import React from "react";
+// import React, { Component } from "react";
+// import SearchForm from "..\SearchForm";
+
+// class Nav extends SearchForm {
+//   render() 
+
+// }
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light" style={{backgroundColor: "#0e35b5"}}>
+    <nav className="navbar navbar-light" style={{backgroundColor: "#0e35b5"}}>
       <a className="navbar-brand" style={{color: "#fff"}} href="/">
+        <img style={{marginRight: "10px"}} src="./BYA-logo-crop.png" height="80" className="d-inline-block align-bottom" alt="logo"></img>
         Bike Your AREA
       </a>
     </nav>
