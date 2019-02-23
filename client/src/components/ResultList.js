8f9dcc0ac7803d2444a15f18e0d26a0865c1caf2
Andrew Swartz <andrewcswartz@gmail.com>
2019-02-23 12:02:02 -0600

    Result Cards

--- client/src/components/ResultList.js
+++ client/src/components/ResultList.js
@@ -27,7 +27,7 @@ function ResultList(props) {
   props.results.map((result) => {
     var imgUrl = ''
 
-    var bikes = ['https://imageos.giant-bicycles.com/b_white,c_pad,h_400,q_80/Defy-Advanced-2-Color-A-Neon-Green/Defy-Advanced-2-Color-A-Neon-Green.jpg', 'https://cdn.shopify.com/s/files/1/0116/6732/products/chief_moss_grn_1024x1024.png?v=1479315448', 'https://cdn.gearpatrol.com/wp-content/uploads/2018/02/Priority-Revamped-Cruiser-gear-patrol-full-lead.jpg']
+    var bikes = ['https://imageos.giant-bicycles.com/b_white,c_pad,h_400,q_80/Defy-Advanced-2-Color-A-Neon-Green/Defy-Advanced-2-Color-A-Neon-Green.jpg','https://www.danflahertylaw.com/images/plainfield-bicycle-accident-attorney.jpg','https://wrongfuldeathsite.files.wordpress.com/2017/06/bicycle-collision.jpg','https://starecat.com/content/wp-content/uploads/trump-riding-on-a-lion-with-putin-riding-on-a-bear.jpg;' ,'http://farm1.staticflickr.com/189/486212501_4eb23facdb.jpg','https://cdn.shopify.com/s/files/1/0116/6732/products/chief_moss_grn_1024x1024.png?v=1479315448', 'https://cdn.gearpatrol.com/wp-content/uploads/2018/02/Priority-Revamped-Cruiser-gear-patrol-full-lead.jpg']
 
     if(result.media.image_url) {
       imgUrl = result.media.image_url
@@ -36,20 +36,21 @@ function ResultList(props) {
       imgUrl = bikes[random]
     }
     var html = (
-        <div className="card" style={{width: '18rem', display: 'inline-block', margin: '10px' }}>
+        <div className="card" style={{width: '18rem', display: 'inline-block', margin: '10px',color: '#ffff'}}>
           <img className="card-img-top" width="200px" height="150px" src={imgUrl}></img>
-          <div className="card-body" style={{maxHeight: '40rem'}} >
+          <div className="card-body" style={{maxHeight: '40rem',backgroundColor: '#3f51b5'}} >
             <h5 className="card-title"style={{ maxHeight: '50px', minHeight: '50px'}}>{result.title}</h5>
             <p>{result.type}</p>
+            
             <p className="card-text" style={{ maxWidth: '250px', overflow: 'scroll', maxHeight: '100px', minHeight: '100px'}}> {result.description}</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
+            {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
           </div>
         </div>
     )   
     results.push(html) 
   }) 
   return (
-    <div style={{ width: '64%' }}>
+    <div style={{ width: '80%' }}>
       {results}
     </div>
   );
