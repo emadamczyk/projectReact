b1bddbe9810e0d3341fc8d09653097f91c794eec
Worm6 <rtswartz6@gmail.com>
2019-02-23 13:35:26 -0600

    Passport working

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,10 @@
 const router = require("express").Router();
 const incidentRoutes = require("./incidents");
 const userRoutes = require("./user");
-const loginRoutes = require("./login")
 
 // incident routes
 router.use("/incidents", incidentRoutes);
 router.use("/user", userRoutes);
-router.use("/login", loginRoutes);
 
 
 
--- routes/api/login.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const passport = require("passport");
-const userController = require("../../controllers/userController");
-
-router.post("/", passport.authenticate("local"), userController.login)
-
-module.exports = router;
